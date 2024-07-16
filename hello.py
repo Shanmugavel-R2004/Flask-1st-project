b110f7df023a52332f2090492d99703d0e52b095
Shanmugavel R <shanmugavelramsamy@gmail.com>
2024-07-16 10:46:09 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return "<div style='background:blue'><h1>Hello world</h1><br><h1>Hii</h1></div>"
+    return "div><h1>Hello world</h1><br><h1>Hii</h1></div"
 if __name__=='__main__':
     app.run(debug=True)
